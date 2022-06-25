bdd154888709ee8dd724fe5a508fb3260d328efb
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-06-25 22:14:00 +0800

    Initial setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const withPreact = require("next-plugin-preact");
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = withPreact(nextConfig);
