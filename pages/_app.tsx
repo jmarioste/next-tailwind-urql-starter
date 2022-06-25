ec04b291b551c234f19bea6e577649162531a5d7
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-06-25 22:50:23 +0800

    Add next-auth and apollo setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+
+import MyApolloProvider from "lib/apollo/ApolloProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider>
+      <MyApolloProvider>
+        <Component {...pageProps} />
+      </MyApolloProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
